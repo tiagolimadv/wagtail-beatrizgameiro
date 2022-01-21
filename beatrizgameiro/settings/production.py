33620314a4e70aec86ee4f2534d28f14ec9bb6fd
Tiago Lima <tiagolimadv@gmail.com>
2022-01-20 23:15:23 -0300

    Deploy Project

--- beatrizgameiro/settings/production.py
+++ beatrizgameiro/settings/production.py
@@ -1,6 +1,8 @@
 from .base import *
 
-DEBUG = False
+DEBUG = int(os.environ.get("DEBUG", default=1))
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALLOWED_HOSTS = os.environ.get("DJANG_ALLOWED_HOSTS").split(" ")
 
 try:
     from .local import *
